06404436eb9abcb26b3e414d876e58edfe18ee95
Rifat7432 <mdr341762@gmail.com>
2024-11-18 09:52:24 +0600

    some init set up

---
+++ src/app/config/index.ts
@@ -0,0 +1,11 @@
+import dotenv from "dotenv";
+import Path from "path";
+dotenv.config({ path: Path.join((process.cwd(), ".env")) });
+
+export default {
+  NODE_ENV: process.env.NODE_ENV,
+  port: process.env.PORT,
+  bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS,
+  jwt_access_secret: process.env.JWT_ACCESS_TOKEN,
+  jwt_access_expires_in: process.env.JWT_ACCESS_EXPIRES_IN,
+};
---
+++ src/server.ts
@@ -0,0 +1,29 @@
+import { Server } from "http";
+import config from "./app/config";
+import app from "./app";
+
+let server: Server;
+
+async function main() {
+  try {
+    server = app.listen(config.port, () => {
+      console.log(`Assignment 6 server is listening on port ${config.port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+main();
+process.on("unhandledRejection", () => {
+  if (server) {
+    server.close(() => {
+      console.log("unahandledRejection is detected , shutting down ...`, err");
+      process.exit(1);
+    });
+  }
+});
+process.on("uncaughtException", () => {
+  console.log("uncaughtException");
+  process.exit(1);
+});
