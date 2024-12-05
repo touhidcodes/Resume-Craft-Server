6b0e8b323552f8391e89c208e5cee3e7b1dccd1a
itamim-coder <itamim12202@gmail.com>
2024-12-06 00:33:56 +0600

    [update] delete  resume

--- src/app/module/Resume/resume.controller.ts
+++ src/app/module/Resume/resume.controller.ts
@@ -46,9 +46,23 @@ const updateResume = catchAsync(async (req, res) => {
   });
 });
 
+const deleteResume = catchAsync(async (req, res) => {
+  const result = await resumeServices.deleteUserResumeFromDB(
+    req.params.resumeId,
+    req.user.userId
+  );
+  sendRes(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Resume Deleted successfully',
+    data: result,
+  });
+});
+
 export const resumeControllers = {
   createResume,
   getResume,
   getAllUserResume,
   updateResume,
+  deleteResume,
 };
